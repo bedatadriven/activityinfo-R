178706a91351197df588a3c6916c5b4bf4d94e99
Alex Bertram <alex@bedatadriven.com>
2016-05-23 10:30:55 +0200

    Fixed CI script

--- inst/ci-build.R
+++ inst/ci-build.R
@@ -8,10 +8,12 @@ install.dependencies <- function() {
   
   # Set a CRAN mirror to use
   options(repos=structure(c(CRAN="http://cran.rstudio.com")))
-  cat("...installed packages: ", paste(installed.packages(), collapse = ", "), "\n")
+  
+  installed <- installed.packages()
+
   # Install CRAN packages
   for(pkg in c('devtools', 'httr', 'rjson', 'RUnit', 'roxygen2', 'Rcpp')) {
-    if(!(pkg %in% installed.packages())) {
+    if(!(pkg %in% installed[, "Package"])) {
       install.packages(pkg, dependencies = TRUE)
     }
   }

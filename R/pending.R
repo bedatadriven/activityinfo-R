c59c9a58d9bbe461347665f2f3b9e325307d9b38
Alexander Bertram <alex@bedatadriven.com>
2022-06-03 17:30:31 +0200

    Add missing submitPending() soruce

---
+++ R/pending.R
@@ -0,0 +1,24 @@
+
+#' Imports a pending change set exported from an ActivityInfo offline database.
+#' 
+#' If a device is no longer able to connect to the internet, you can export
+#' the changes queued for synchronization and manually transfer the pending.json
+#' file to the database owner, who can use this send the changes to the server
+#' under their own account.
+#' 
+#' @export
+submitPending <- function(file.name) {
+  pending <- fromJSON(file = file.name)
+  for(p in pending) {
+    if(is.null(p$transaction)) next
+    updateSet <- p$transaction
+    # fix up object for RJSON :-(
+    for(i in seq_along(updateSet$changes)) {
+      if(is.null(updateSet$changes[[i]]$parentRecordId)) {
+        # Remove the property, otherwise rjson turns it into {} rather than null
+        updateSet$changes[[i]]$parentRecordId <- NULL
+      }
+    }
+    postResource("update/offline", updateSet)
+  }
+}

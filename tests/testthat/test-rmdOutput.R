103195d27ad59fa41d6a1f04351b97c6556f4683
Nicolas Dickinson <dickinson@washnote.com>
2022-12-12 12:16:42 +0100

    Correcting version number in feature branch and rmd check fixes

--- tests/testthat/test-rmdOutput.R
+++ tests/testthat/test-rmdOutput.R
@@ -1,8 +1,8 @@
 testthat::test_that("Rmd outputs as expected", {
   testthat::expect_no_warning(
     testthat::expect_no_error(
-      rmarkdown::render(here::here("tests/testthat/ext/TestMessages.Rmd"))
+      rmarkdown::render(testthat::test_path("ext/TestMessages.Rmd"))
     )
   )
-  testthat::expect_snapshot_file(here::here("tests/testthat/ext/TestMessages.html"))
+  testthat::expect_snapshot_file(testthat::test_path("ext/TestMessages.html"))
 })

cc488412c6bd842163f91162cabc5135dcefc1ea
Nicolas Dickinson <dickinson@washnote.com>
2023-02-06 11:39:14 +0100

    First draft of form field functions

--- R/formField.r
+++ R/formField.r
@@ -13,14 +13,26 @@
 #' @param required Whether the form field is required; default is FALSE
 #' @param hideFromEntry Whether the form field is hidden during data entry; default is FALSE
 #' @param hideInTable Whether the form field is hidden during data display; default is FALSE
-#' @param relevanceRules Relevance rules for the form field
-#' @param validationRules Validation rules for the form field
+#' @param relevanceRules Relevance rules for the form field given as a character string; default is ""
+#' @param validationRules Validation rules for the form field given as a character string; default is ""
 #' @param reviewerOnly Whether the form field is for reviewers only; default is FALSE
 #' @param typeParameters The type parameters object specific to the type given.
 #'
 #' @export
 formFieldSchema <- function(type, label, description = NULL, code = NULL, id = cuid(), key = FALSE, required = FALSE, hideFromEntry = FALSE, hideInTable = FALSE, relevanceRules = "", validationRules = "", reviewerOnly = FALSE, typeParameters = NULL) {
-
+  stopifnot("The label is required to be a character string" = (is.character(label)&&length(label)>1&&nchar(label)>0))
+  stopifnot("The description must be a character string" = is.null(description)||(is.character(description)&&length(description)>1&&nchar(description)>0))
+  stopifnot("The code must be a character string" = is.null(code)||(is.character(code)&&length(code)>1&&nchar(code)>0))
+  stopifnot("The id is required and must be a character string" = !is.null(id)&&(is.character(id)&&length(id)>1&&nchar(id)>0))
+  stopifnot("`relevanceRules` must be given as a character string" = !is.null(relevanceRules)&&(is.character(relevanceRules)&&length(relevanceRules)>1))
+  stopifnot("`validationRules` must be given as a character string" = !is.null(validationRules)&&(is.character(validationRules)&&length(validationRules)>1))
+  stopifnot("The key must be a logical/boolean of length 1" = is.logical(key)&&length(key)==1)
+  stopifnot("`required` must be a logical/boolean of length 1" = is.logical(required)&&length(required)==1)
+  stopifnot("`hideFromEntry` must be a logical/boolean of length 1" = is.logical(hideFromEntry)&&length(hideFromEntry)==1)
+  stopifnot("`hideInTable` must be a logical/boolean of length 1" = is.logical(hideInTable)&&length(hideInTable)==1)
+  stopifnot("`reviewerOnly` must be a logical/boolean of length 1" = is.logical(reviewerOnly)&&length(reviewerOnly)==1)
+  stopifnot("The type parameters must be a list if defined" = is.null(typeParameters)||is.list(typeParameters))
+  
   schema <- list()
   
   schema$id <- id
@@ -32,9 +44,7 @@ formFieldSchema <- function(type, label, description = NULL, code = NULL, id = c
   schema$label <- label
   schema$relevanceCondition <- relevanceRules
   schema$validationCondition <- validationRules
-  
   schema$tableVisible <- !hideFromEntry
-  
   schema$required <- required
   
   if (!is.null(description)) {
@@ -81,13 +91,6 @@ addFormFieldSchemaCustomClass <- function(e) {
   return(e)
 }
 
-validateFormFieldSchema <- function(schema) {
-  stopifnot("activityInfoFormFieldSchema" %in% class(schema))
-  # check if has cuid
-  # check if has all standard properties
-  # ...
-}
-
 #' Pretty print a form field schema
 #'
 #' @param x an object of class \code{activityInfoFormFieldSchema}.
@@ -131,6 +134,10 @@ print.activityInfoFormFieldSchema <- function(x, ...) {
 }
 
 #' Create a text form field schema
+#' 
+#' You can define the format of the text that the users should type in a Text 
+#' field using an Input Mask. 
+#' See: https://www.activityinfo.org/support/docs/m/93526/l/1143998.html
 #' 
 #' @inheritParams formFieldSchema
 #'
@@ -193,7 +200,12 @@ validateBarcodeFieldSchema <- function(schema) {
   stopifnot("A barcode Field Schema must have the type parameter barcode = TRUE." = identical(schema$typeParameters$barcode, TRUE))
 }
 
-#' Create a text form field schema
+#' Create a serial number form field schema
+#' 
+#' Only one serial number field is possible in a form. The Prefix Formula is 
+#' available for Serial Number fields and can be used to customise how the 
+#' Serial Number will appear. In Subforms, the Prefix Formula can also derive 
+#' from the Parent Form.
 #' 
 #' @param digits The number of digits in the serial number
 #' @param prefixFormula A formula as a character string defining the prefix for 
@@ -230,6 +242,460 @@ validateSerialNumberFieldSchema <- function(schema) {
     stopifnot(is.character(schema$typeParameters$prefixFormula)&&length(schema$typeParameters$prefixFormula)==1)
   }
 }
+
+
+#' Create a quantity form field schema
+#' 
+#' A Quantity field allow users to enter a numerical value. You can define the 
+#' units and the aggregation function.
+#' 
+#' A quantity field cannot be a key field.
+#' 
+#' @inheritParams formFieldSchema
+#' @param units A character string describing the units, e.g. "litres per day"
+#' @param aggregation A character string giving the aggregation function; "SUM" 
+#' is default
+#'
+#' @export
+quantityFieldSchema <- function(label, description = NULL, units = "", aggregation = "SUM", code = NULL, id = cuid(), required = FALSE, hideFromEntry = FALSE, hideInTable = FALSE, relevanceRules = "", validationRules = "", reviewerOnly = FALSE) {
+  stopifnot("Units must be a character string (empty or not)" = is.character(units)&&length(units)==1)
+  stopifnot("Aggregation must be a character string" = is.character(aggregation)&&length(aggregation)==1)
+  schema <- do.call(
+    formFieldSchema, 
+    args = c(
+      list(type = "quantity"),
+      as.list(environment()),
+      list(
+        typeParameters = list(
+          "units" = units,
+          "aggregation" = aggregation
+          )
+      )
+    )
+  )
+  
+  schema
+}
+
+#' Create a multi-line or narrative form field schema
+#' 
+#' Multi-Line Text fields can be used to collect long answers to open-ended 
+#' questions. They could be used for example to collect Comments about a 
+#' specific Form or an Extended Narrative. It cannot be a key field.
+#' 
+#' @inheritParams formFieldSchema
+#'
+#' @export
+multilineFieldSchema <- function(label, description = NULL, code = NULL, id = cuid(), required = FALSE, hideFromEntry = FALSE, hideInTable = FALSE, relevanceRules = "", validationRules = "", reviewerOnly = FALSE) {
+  schema <- do.call(
+    formFieldSchema, 
+    args = c(
+      list(type = "NARRATIVE"),
+      as.list(environment())
+    )
+  )
+  
+  schema
+}
+
+#' Create a date form field schema
+#' 
+#' The Date format in ActivityInfo is YYYY-MM-DD so no matter the way the Date 
+#' is typed by a user it will always appear in this format.
+#' 
+#' @inheritParams formFieldSchema
+#'
+#' @export
+dateFieldSchema <- function(label, description = NULL, code = NULL, id = cuid(), key = FALSE, required = FALSE, hideFromEntry = FALSE, hideInTable = FALSE, relevanceRules = "", validationRules = "", reviewerOnly = FALSE) {
+  schema <- do.call(
+    formFieldSchema, 
+    args = c(
+      list(type = "date"),
+      as.list(environment())
+    )
+  )
+  
+  schema
+}
+
+#' Create a week form field schema
+#' 
+#' The Week format in ActivityInfo is YYYY-WW. Users can directly type using 
+#' this format or use the calendar to select a week. Please note that the Week 
+#' field uses the EPI week convention.
+#' 
+#' @inheritParams formFieldSchema
+#'
+#' @export
+weekFieldSchema <- function(label, description = NULL, code = NULL, id = cuid(), key = FALSE, required = FALSE, hideFromEntry = FALSE, hideInTable = FALSE, relevanceRules = "", validationRules = "", reviewerOnly = FALSE) {
+  schema <- do.call(
+    formFieldSchema, 
+    args = c(
+      list(type = "epiweek"),
+      as.list(environment())
+    )
+  )
+  
+  schema
+}
+
+#' Create a fortnight form field schema
+#' 
+#' The Fortnight field format in ActivityInfo is YYYYWW-WW (e.g. 2020W3-W4). 
+#' Users can directly type using this format or use the calendar to select a 
+#' week.
+#' 
+#' @inheritParams formFieldSchema
+#'
+#' @export
+fortnightFieldSchema <- function(label, description = NULL, code = NULL, id = cuid(), key = FALSE, required = FALSE, hideFromEntry = FALSE, hideInTable = FALSE, relevanceRules = "", validationRules = "", reviewerOnly = FALSE) {
+  schema <- do.call(
+    formFieldSchema, 
+    args = c(
+      list(type = "fortnight"),
+      as.list(environment())
+    )
+  )
+  
+  schema
+}
+
+#' Create a month form field schema
+#' 
+#' The Month format in ActivityInfo is YYYY-MM.
+#' 
+#' @inheritParams formFieldSchema
+#'
+#' @export
+monthFieldSchema <- function(label, description = NULL, code = NULL, id = cuid(), key = FALSE, required = FALSE, hideFromEntry = FALSE, hideInTable = FALSE, relevanceRules = "", validationRules = "", reviewerOnly = FALSE) {
+  schema <- do.call(
+    formFieldSchema, 
+    args = c(
+      list(type = "month"),
+      as.list(environment())
+    )
+  )
+  
+  schema
+}
+
+selectFieldSchema <- function(cardinality, label, description = NULL, values = list(), presentation = "automatic", code = NULL, id = cuid(), key = FALSE, required = FALSE, hideFromEntry = FALSE, hideInTable = FALSE, relevanceRules = "", validationRules = "", reviewerOnly = FALSE) {
+  stopifnot("Presentation must be a character string" = is.character(presentation)&&length(presentation)==1)
+  schema <- do.call(
+    formFieldSchema, 
+    args = c(
+      list(type = "enumerated"),
+      as.list(environment()),
+      list(
+        typeParameters = list(
+          "cardinality" = cardinality,
+          "presentation" = presentation,
+          "values" = values
+        )
+      )
+    )
+  )
+  
+  schema
+}
+
+#' Create a Single Select form field schema
+#' 
+#' There is an options parameter for the list of single select items. Single 
+#' Selection fields can be used to ask from users to select one out of two or 
+#' more options as an answer. This can be a "Yes" or "No" question for example 
+#' or any other type of question that can be answered with one option. Single 
+#' Selection fields can be used in Prefix Formulas to customize Serial Numbers 
+#' too.
+#' 
+#' @inheritParams formFieldSchema
+#' @param values A list of the single select field values
+#' @param presentation Default is "automatic"
+#'
+#' @export
+singleSelectFieldSchema <- function(label, description = NULL, values = list(), presentation = "automatic", code = NULL, id = cuid(), key = FALSE, required = FALSE, hideFromEntry = FALSE, hideInTable = FALSE, relevanceRules = "", validationRules = "", reviewerOnly = FALSE) {
+  schema <- do.call(
+    selectFieldSchema, 
+    args = c(
+      list(cardinality = "single"),
+      as.list(environment())
+    )
+  )
+}
+
+#' Create a Multiple Select form field schema
+#' 
+#' There is an options parameter for the list of multiple select items. Multiple 
+#' Selection fields can be used to ask from users to select any number of 
+#' options as an answer.
+#' 
+#' @inheritParams formFieldSchema
+#' @param values A list of the multiple select field values
+#' @param presentation Default is "automatic"
+#'
+#' @export
+multipleSelectFieldSchema <- function(label, description = NULL, values = list(), presentation = "automatic", code = NULL, id = cuid(), key = FALSE, required = FALSE, hideFromEntry = FALSE, hideInTable = FALSE, relevanceRules = "", validationRules = "", reviewerOnly = FALSE) {
+  schema <- do.call(
+    selectFieldSchema, 
+    args = c(
+      list(cardinality = "multiple"),
+      as.list(environment())
+    )
+  )
+}
+
+#' Converts a factor, list or vector to select values
+#' 
+#' Creates a list of values in the ActivityInfo format for Single Select or
+#' Multiple Select form fields.
+#' 
+#' @rdname toSelectValues
+#' @param values The values to convert
+#'
+#' @export
+toSelectValues <- function(values) {
+  UseMethod("toSelectValues")
+}
+
+
+#' @rdname toSelectValues
+#' @export
+toSelectValues.character <- function(values) {
+  lapply(
+    values,
+    function(x) {
+      list(
+        id = cuid(), 
+        label = x)
+      })
+}
+
+#' @rdname toSelectValues
+#' @export
+toSelectValues.default <- toSelectValues.character
+
+
+#' @rdname toSelectValues
+#' @export
+toSelectValues.list <- function(values) {
+  values <- as.character(values)
+  toSelectValues.character(values)
+}
+
+#' @rdname toSelectValues
+#' @export
+toSelectValues.factor <- function(values) {
+  toSelectValues.character(levels(values))
+}
+
+#' Create an attachments form field schema
+#' 
+#' An attachments field allow users to add one or more attachments.
+#' 
+#' An attachments field cannot be a key field.
+#' 
+#' @inheritParams formFieldSchema
+#'
+#' @export
+attachmentsFieldSchema <- function(label, description = NULL, code = NULL, id = cuid(), required = FALSE, hideFromEntry = FALSE, hideInTable = FALSE, relevanceRules = "", validationRules = "", reviewerOnly = FALSE) {
+  schema <- do.call(
+    formFieldSchema, 
+    args = c(
+      list(type = "attachment"),
+      as.list(environment()),
+      list(
+        typeParameters = list(
+          "cardinality" = multiple,
+          "kind" = "attachment"
+        )
+      )
+    )
+  )
+  
+  schema
+}
+
+
+#' Create a calculated field schema
+#' 
+#' A calculated field can be used to define a formula that presents calculation 
+#' results in the form.
+#' 
+#' A calculated field cannot be a key field.
+#' 
+#' @inheritParams formFieldSchema
+#' @param formula A character string with the calculation formula
+#'
+#' @export
+calculatedFieldSchema <- function(label, description = NULL, formula, code = NULL, id = cuid(), hideFromEntry = FALSE, hideInTable = FALSE, relevanceRules = "", validationRules = "", reviewerOnly = FALSE) {
+  stopifnot("Formula must be a character string" = is.character(formula)&&length(formula)==1&&nchar(formula)>1)
+  schema <- do.call(
+    formFieldSchema, 
+    args = c(
+      list(type = "calculated"),
+      as.list(environment()),
+      list(
+        typeParameters = list(
+          "formula" = formula
+        )
+      )
+    )
+  )
+  
+  schema
+}
+
+#' Create a Subform field schema
+#' 
+#' A subform field can be used to define a field that contains a subform.
+#' 
+#' A subform field cannot be a key field.
+#' 
+#' @inheritParams formFieldSchema
+#' @param subformId The id of the sub-form
+#'
+#' @export
+subformFieldSchema <- function(label, description = NULL, subformId, code = NULL, id = cuid(), hideFromEntry = FALSE, hideInTable = FALSE, relevanceRules = "", validationRules = "", reviewerOnly = FALSE) {
+  stopifnot("The subform id must be a character string" = is.character(subformId)&&length(subformId)==1&&nchar(subformId)>1)
+  schema <- do.call(
+    formFieldSchema, 
+    args = c(
+      list(type = "subform"),
+      as.list(environment()),
+      list(
+        typeParameters = list(
+          "formId" = subformId
+        )
+      )
+    )
+  )
+  
+  schema
+}
+
+#' Create a Reference field schema
+#' 
+#' A reference field can be used to make reference to a record in another form.
+#' 
+#' @inheritParams formFieldSchema
+#' @param formId The id of the referenced form
+#'
+#' @export
+referenceFieldSchema <- function(label, description = NULL, formId, code = NULL, id = cuid(), key = FALSE, hideFromEntry = FALSE, hideInTable = FALSE, relevanceRules = "", validationRules = "", reviewerOnly = FALSE) {
+  stopifnot("The referenced form id must be a character string" = is.character(formId)&&length(formId)==1&&nchar(formId)>1)
+  schema <- do.call(
+    formFieldSchema, 
+    args = c(
+      list(type = "reference"),
+      as.list(environment()),
+      list(
+        typeParameters = list(
+          "cardinality" = "single",
+          "formId" = formId
+        )
+      )
+    )
+  )
+  
+  schema
+}
+
+#' Create a Geographic Point form field schema
+#' 
+#' A Geographic Point field allow users to enter a geo-location with a certain
+#' accuracy.
+#' 
+#' The user interface as the required geo-location accuracy options:
+#' * No minimum accuracy
+#' * Medium accuracy (25 m); default
+#' * High accuracy (10 m)"
+#' 
+#' This field cannot be a key field.
+#' 
+#' @inheritParams formFieldSchema
+#' @param requiredAccuracy Minimum required accuracy in metres/meters.
+#' @param manualEntryAllowed Whether coordinates may be added manually; default
+#' is TRUE
+#'
+#' @export
+geopointFieldSchema <- function(label, description = NULL, requiredAccuracy = NULL, manualEntryAllowed = TRUE, code = NULL, id = cuid(), required = FALSE, hideFromEntry = FALSE, hideInTable = FALSE, relevanceRules = "", validationRules = "", reviewerOnly = FALSE) {
+  stopifnot("requiredAccuracy must be a single numeric value or NULL" = is.null(requiredAccuracy)||(is.numeric(requiredAccuracy)&&length(requiredAccuracy)==1))
+  stopifnot("manualEntryAllowed must be single logical" = is.logical(manualEntryAllowed)&&length(manualEntryAllowed)==1)
+  
+  schemaArgs <- c(
+      list(type = "geopoint"),
+      as.list(environment()),
+      list(
+        typeParameters = list(
+          "aggregation" = aggregation
+        )
+      )
+      )
+      
+  schemaArgs$typeParameters$requiredAccuracy <- requiredAccuracy
+  
+  schema <- do.call(
+    formFieldSchema, 
+    args = schemaArgs
+    )
+  
+  schema
+}
+
+#' Create a User form field schema
+#' 
+#' User fields allow you to select a specific user from a list. It is a field 
+#' useful for the Case Management Database template with which you can assign 
+#' Cases to specific users. User fields and inactive users: If you are using 
+#' the User field in your Form and a user who has added Records to the Form, is 
+#' no longer active, instead of deleting that user, create a new Role empty of 
+#' permissions and assign it to that user.
+#' 
+#' 
+#' @inheritParams formFieldSchema
+#' @param databaseId The database id of the form and users
+#'
+#' @export
+userFieldSchema <- function(label, description = NULL, databaseId, code = NULL, id = cuid(), key = FALSE, required = FALSE, hideFromEntry = FALSE, hideInTable = FALSE, relevanceRules = "", validationRules = "", reviewerOnly = FALSE) {
+  stopifnot("`databaseId` must be a character string" = is.character(databaseId)&&length(databaseId)==1&&nchar(databaseId)>0)
+  schema <- do.call(
+    formFieldSchema, 
+    args = c(
+      list(type = "reference"),
+      as.list(environment()),
+      list(
+        typeParameters = list(
+          "cardinality" = "single",
+          "range" = list(list(
+            "formId" = sprintf("%s@users", databaseId)
+          ))
+        )
+      )
+    )
+  )
+  
+  schema
+}
+
+#' Create a section header form field schema
+#' 
+#' A special form field to define a section header for the form.
+#' 
+#' @inheritParams formFieldSchema
+#'
+#' @export
+sectionFieldSchema <- function(label, description = NULL) {
+  schema <- do.call(
+    formFieldSchema, 
+    args = c(
+      list(type = "section"),
+      as.list(environment())
+    )
+  )
+  
+  schema
+}
+
 
 #' Add a new form field
 #' 

7b78282d148621e0d021b7ca86cbf95ee509c3a8
Alex Bertram <alex@bedatadriven.com>
2023-05-24 10:41:10 +0200

    Fix for audit API test affected by AI-3437

--- tests/testthat/test-audit.R
+++ tests/testthat/test-audit.R
@@ -3,7 +3,7 @@ testthat::test_that("queryAuditLog() works", {
   results <- queryAuditLog(database$databaseId, limit = 1000)
 
   testthat::expect_gte(nrow(results), 15)
-  testthat::expect_equal(names(results), c("id", "time", "recordRef", "version", "databaseUserId", "description", "type", "deleted", "reverted", "user.id", "user.name", "user.email"))
+  testthat::expect_true(all(c("id", "time", "recordRef", "version", "databaseUserId", "description", "type", "deleted", "reverted", "user.id", "user.name", "user.email") %in% names(results)))
 
 })
 

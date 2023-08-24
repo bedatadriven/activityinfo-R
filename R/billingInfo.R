cf4d6fc2825c862106f5f5b486c3004617355ff2
Ryo Nakagawara <ryonakagawara@gmail.com>
2023-08-24 22:23:32 +0900

    - rename functions in current package style - use billingId instead of userId for specifying ID to the billing functions

--- R/billingInfo.R
+++ R/billingInfo.R
@@ -1,14 +1,14 @@
-#' @title getBillingInfo
+#' @title getBillingAccount
 #' @description Get billing account information
 #' @param asDataFrame Output as data.frame, Default: TRUE
-#' @param userId User ID
+#' @param billingId Billing ID
 #' @return Billing account information in list or data.frame output
-#' @rdname getBillingInfo
+#' @rdname getBillingAccount
 #' @export 
 #' @importFrom tibble as_tibble
 
-getBillingInfo <- function(asDataFrame = TRUE, userId) {
-  billingInfo <- getResource(paste0("/billingAccounts/", userId), task = "Getting billing account info")
+getBillingAccount <- function(asDataFrame = TRUE, billingId) {
+  billingInfo <- getResource(paste0("/billingAccounts/", billingId), task = "Getting billing account info")
   if (asDataFrame == TRUE) {
     billingInfo <- tibble::as_tibble(billingInfo)
     return(billingInfo)
@@ -18,17 +18,17 @@ getBillingInfo <- function(asDataFrame = TRUE, userId) {
 }
 
 
-#' @title getBillingDatabases
+#' @title getBillingAccountDatabases
 #' @description Data for all databases under billing account
 #' @param asDataFrame Output as data.frame, Default: TRUE
-#' @param userId User ID
+#' @param billingId Billing ID
 #' @return Information on databases under billing account in list or data.frame output
-#' @rdname getBillingDatabases
+#' @rdname getBillingAccountDatabases
 #' @export 
 #' @importFrom tibble tibble
 
-getBillingDatabases <- function(asDataFrame = TRUE, userId) {
-  billingDatabases <- getResource(paste0("/billingAccounts/", userId, "/databases"), 
+getBillingAccountDatabases <- function(asDataFrame = TRUE, billingId) {
+  billingDatabases <- getResource(paste0("/billingAccounts/", billingId, "/databases"), 
                                   task = "Getting billing account databases")
   if (asDataFrame == TRUE) {
     billingDatabases <- tibble::tibble(
@@ -52,33 +52,33 @@ getBillingDatabases <- function(asDataFrame = TRUE, userId) {
 }
 
 
-#' @title getBillingDomains
+#' @title getBillingAccountDomains
 #' @description Billing account email domain info
-#' @param userId User ID
+#' @param billingId Billing ID
 #' @return Information on billing account email domain in list output
-#' @rdname getBillingDomains
+#' @rdname getBillingAccountDomains
 #' @export 
 
-getBillingDomains <- function(userId) {
-  billingDomains <- getResource(paste0("/billingAccounts/", userId, "/domains"), task = "Getting billing account domains")
+getBillingAccountDomains <- function(billingId) {
+  billingDomains <- getResource(paste0("/billingAccounts/", billingId, "/domains"), task = "Getting billing account domains")
   return(billingDomains)
 }
 
 
-#' @title getBillingUsers
+#' @title getBillingAccountUsers
 #' @description Billing account users
 #' @param asDataFrame Output as data.frame, Default: TRUE
-#' @param userId User ID
+#' @param billingId Billing ID
 #' @return Billing account user(s) in list or data.frame output
-#' @rdname getBillingUsers
+#' @rdname getBillingAccountUsers
 #' @export 
 #' @importFrom tibble tibble
 
-getBillingUsers <- function(asDataFrame = TRUE, userId) {
-  billingUsers <- getResource(paste0("/billingAccounts/", userId, "/users"), task = "Getting billing account users")
+getBillingAccountUsers <- function(asDataFrame = TRUE, billingId) {
+  billingUsers <- getResource(paste0("/billingAccounts/", billingId, "/users"), task = "Getting billing account users")
   if (asDataFrame == TRUE) {
     billingUsers <- tibble::tibble(
-      userId = unlist(lapply(billingUsers, function(x) {x$userId})),
+      billingId = unlist(lapply(billingUsers, function(x) {x$billingId})),
       email = unlist(lapply(billingUsers, function(x) {x$email})),
       name = unlist(lapply(billingUsers, function(x) {x$name})),
       billingAccountRole = unlist(lapply(billingUsers, function(x) {x$billingAccountRole})),
@@ -93,35 +93,35 @@ getBillingUsers <- function(asDataFrame = TRUE, userId) {
 }
 
 
-#' @title getDatabaseOwner
+#' @title getDatabaseBillingAccount
 #' @description Get database owner for a given database. This gives the owning organization rather than the user who created the database.
 #' @param asDataFrame Data.frame output, Default: TRUE
 #' @param databaseId Database ID
 #' @return Database owner in list output
-#' @rdname getDatabaseOwner
+#' @rdname getDatabaseBillingAccount
 #' @export 
 
-getDatabaseOwner <- function(asDataFrame = TRUE, databaseId) {
+getDatabaseBillingAccount <- function(asDataFrame = TRUE, databaseId) {
   databaseOwner <- getResource(paste0("/databases/", databaseId, "/billingAccount"), task = "Getting database owner")
   return(databaseOwner)
 }
 
 
-#' @title getBillingDatabaseUsers
+#' @title getBillingAccountDatabaseUsers
 #' @description Get data for users from a specific database. Can be more useful than `getDatabaseUsers()` as you also retrieve the database owner's info as well.
 #' @param asDataFrame Data.frame output, Default: TRUE
-#' @param userId User ID
+#' @param billingId Billing ID
 #' @param databaseId Database ID
 #' @return User information from the specified database in list or data.frame output
-#' @rdname getBillingDatabaseUsers
+#' @rdname getBillingAccountDatabaseUsers
 #' @export 
 #' @importFrom tibble tibble
 
-getBillingDatabaseUsers <- function(asDataFrame = TRUE, userId, databaseId) {
-  databaseUsers <- getResource(paste0("billingAccounts/", userId, "/users?databaseId=", databaseId), task = "Getting database data")
+getBillingAccountDatabaseUsers <- function(asDataFrame = TRUE, billingId, databaseId) {
+  databaseUsers <- getResource(paste0("billingAccounts/", billingId, "/users?databaseId=", databaseId), task = "Getting database data")
   if (asDataFrame == TRUE) {
     databaseUsers <- tibble::tibble(
-      userId = unlist(lapply(databaseUsers, function(x) {x$userId})),
+      billingId = unlist(lapply(databaseUsers, function(x) {x$billingId})),
       email = unlist(lapply(databaseUsers, function(x) {x$email})),
       name = unlist(lapply(databaseUsers, function(x) {x$name})),
       billingAccountRole = unlist(lapply(databaseUsers, function(x) {x$billingAccountRole})),
